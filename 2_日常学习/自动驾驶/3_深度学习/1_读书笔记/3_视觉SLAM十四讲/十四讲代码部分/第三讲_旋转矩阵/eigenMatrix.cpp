8dbe5d83a28fabc600c259bc32b0cf8b89b2807a
guanggang <1427963351@qq.com>
2025-04-09 07:23:30 +0800

    更新

--- 2_日常学习/自动驾驶/3_深度学习/1_读书笔记/3_视觉SLAM十四讲/十四讲代码部分/第三讲_旋转矩阵/eigenMatrix.cpp
+++ 2_日常学习/自动驾驶/3_深度学习/1_读书笔记/3_视觉SLAM十四讲/十四讲代码部分/第三讲_旋转矩阵/eigenMatrix.cpp
@@ -19,5 +19,36 @@ using namespace std;
 
     // 声明一个 2x3 的 float 矩阵
     Eigen::Matrix<float, 2, 3> matrix_23;
+    // 同时，Eigen通过 typedef 定义了许多内置类型，不过底层仍是 Eigen::Matrix
+    // 例如 Vector3d 实质上时 Eigen::Matrix<double, 3, 1>
+    Eigen::Vector3d v_3d;
+    // 还有 Matrix3d实质上时 Eigen::Matrix3d<double, 3, 3>
+    Eigen::Matrix3d matrix_33 = Eigen::Matrix<double, 3, 3>
+    // 如果不确定矩阵的大小，可以使用动态大小的矩阵
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> matrix_dynamic;
+    // 或者更简单一点
+    Eigen::MatrixXd matrix_x;
+
+    // 下面时对矩阵的操作
+    // 输入数据
+    matrix_23 << 1, 2, 3, 4, 5, 6;
+    // 输出
+    cout << matrix_23 << endl;
+
+    // 使用（）访问矩阵中的元素
+    for (int i = 0; i < 1; i++) {
+      for (int j = 0; j < 2; j++) {
+         cout << matrix_23(i, j) << endl;
+      }
+    }
+
+    v_3d << 3, 2, 1;
+
+    // 矩阵和向量相乘，实际上是矩阵和矩阵相乘，但是不能混和两种不同类型的矩阵，这样写是错的
+    // Eigen::Matrix<double, 2, 1> resutl_wrong_type = matrix_23 * v_3d;
+
+    // 应该通过显式转换, 将 float 类型转化成 double 类型
+    Eigen::Matrix<double, 2, 1> result = matrix_23.cast<double>() * v_3d;
+    
     // 
- }
+}
