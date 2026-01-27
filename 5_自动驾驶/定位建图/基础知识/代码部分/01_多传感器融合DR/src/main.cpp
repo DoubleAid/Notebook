4e2d79652a2f78f5095b6d17e7e5ab440cb33496
guanggang.bian <macbook>
2026-01-27 11:33:42 +0800

    1月27日

--- 5_自动驾驶/定位建图/基础知识/代码部分/01_多传感器融合DR/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <rclcpp/rclcpp.h>
-#include "multi_sensor_dr/node/multi_sensor_dr_node.hpp"
-
-int main(int argc, char** argv) {
-    rclcpp::init(argc. argv);
-    rclcpp::spin(std::shared_ptr<MultiSensorDRNode>());
-    rclcpp::shutdown();
-    return 0;
-}
